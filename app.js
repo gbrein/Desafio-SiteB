95a359ee6c10f1fdd9ffd93677dc79728428d796
gbrein <gbrein@gmail.com>
2019-06-24 18:32:15 -0300

    express setado

--- app.js
+++ app.js
@@ -1,26 +1,17 @@
 var createError = require('http-errors');
-var express = require('express');
+const express = require('express');
+
+const app = express();
+
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var routes = require('./routes/routes');
 
-var app = express();
+app.use('/', routes);
+app.use('/post', routes);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +28,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+
 
 module.exports = app;
