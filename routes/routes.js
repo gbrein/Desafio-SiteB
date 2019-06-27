0f3bac9bd9252fef98deb79c64353e99423363c9
gbrein <gbrein@gmail.com>
2019-06-27 14:14:16 -0300

    tentando criar leitura

--- routes/routes.js
+++ routes/routes.js
@@ -1,27 +1,41 @@
 const express = require('express');
 const router = express.Router();
-const data = require('../data.json');
+let dataBase = require('../data.json');
 const app = express();
 let bodyParser = require('body-parser');
+const fs = require('fs');
+const file = '../data.json'
+ 
 
 var jsonParser = bodyParser.json()
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
 
 router.get('/get', (request, response) => {
-  response.json(data);
+    var content = fs.readFileSync(file)
+    let c = (content);
 });
 
 router.post('/post', jsonParser, (request, response) => {
-  // const newData = request.body.formData;
-  console.log(request.body)
+    let data = request.body;
+    let jsonData = JSON.stringify(data);
+
+    let jsonBase = JSON
+        .stringify(dataBase)
+        .replace("[", "")
+        .replace("]", "") + ',' + jsonData;
+    jsonBase = '[' + jsonBase + ']';
+
+    fs.writeFile('./data.json', jsonBase, (err) => {
+        if (err) 
+            throw err;
+        console.log('The file has been saved!');
+    });
 })
 
 module.exports = router;
-
-
